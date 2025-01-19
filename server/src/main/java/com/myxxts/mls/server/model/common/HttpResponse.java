4d34b53e6bd9e42a31b3e110b72bbc40832b61a3
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-01-19 22:27:13 +0800

    refactor(server): standardize and merge ErrorResponse class and HttpResponse class

--- server/src/main/java/com/myxxts/mls/server/model/common/HttpResponse.java
+++ server/src/main/java/com/myxxts/mls/server/model/common/HttpResponse.java
@@ -1,8 +1,13 @@
 package com.myxxts.mls.server.model.common;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@JsonInclude (JsonInclude.Include.NON_NULL)
 public class HttpResponse<T> {
 
   private int code;
@@ -11,23 +16,23 @@ public class HttpResponse<T> {
 
   private T data;
 
-  public static <T> HttpResponse<T> success (T data) {
+  private LocalDateTime timestamp = LocalDateTime.now();
 
+  public static <T> HttpResponse<T> success (T data) {
     HttpResponse<T> response = new HttpResponse<>();
     response.setCode(200);
     response.setMessage("success");
     response.setData(data);
     return response;
-
   }
 
   public static <T> HttpResponse<T> error (int code, String message) {
-
     HttpResponse<T> response = new HttpResponse<>();
     response.setCode(code);
     response.setMessage(message);
     return response;
-
   }
+
+  private HttpResponse () {}
 
 }
